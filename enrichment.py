b6d33d55ac7fe884c58c3da69c2f5a14f2119ca8
MariaLozano01 <102619474+MariaLozano01@users.noreply.github.com>
2022-09-11 18:02:46 -0400

    Updates

--- enrichment.py
+++ enrichment.py
@@ -1,4 +1,21 @@
 import pandas as pd
+adi = pd.read_csv('US_2019_ADI_Census Block Group_v3.1.csv.zip')
+adi
+import pandas as pd
+discharges = pd.read_csv('Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv.zip')
+discharges
 
-Discharges = pd.read_csv('Hospital_Inpatient_Discharges__SPARCS_De-Identified___2015.csv')
+discharges.columns
+adi.columns
+discharges['Zip Code - 3 digits']
+adi['FIPS']
 
+df_adi_small = adi(('Unnamed: 0', 'GISJOIN', 'FIPS', 'ADI_NATRANK', 'ADI_STATERNK'))
+print[df_adi_small.sample(10).to_markdown()] 
+
+df_discharges_small = discharges(('Health Service Area', 'Hospital County', 'Facility Id', 'Facility Name', 'Zip Code - 3 digits', 'Total Charges', 'Total Costs'))
+print[df_discharges_small.sample(10).to_markdown()] 
+
+combined_df = pd.merge(df_discharges_small, df_adi_small, how= 'left', left_on= 'FIPS', right_on= 'Zip Code - 3 digits')
+
+ 
